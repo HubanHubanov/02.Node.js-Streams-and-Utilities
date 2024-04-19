e1c7c80b8e316aed709976e34fe38cf6b3dbab7c
HubanHubanov <huban.hubanov@gmail.com>
2024-04-19 17:55:16 +0300

    add eventsEmitter

---
+++ events/index.js
@@ -0,0 +1,8 @@
+const events = require("events");
+const eventEmitter = new events.EventEmitter();
+
+eventEmitter.on("request", (eventData) => {
+    console.log("On request - " + eventData);
+});
+
+eventEmitter.emit("request", "Request emitted")
