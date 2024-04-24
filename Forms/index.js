aaa16aa0ecfd68de534e3355140ad32e3a500fe5
HubanHubanov <huban.hubanov@gmail.com>
2024-04-24 20:38:59 +0300

    add Forms

---
+++ Forms/index.js
@@ -0,0 +1,104 @@
+// Have to change the dir in html between upload, upload2 and /
+const http = require("http");
+const querystring = require("querystring");
+const path = require("path");
+const fs = require("fs");
+const { EOL } = require("os");
+const formidable = require("formidable");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+
+    res.write(`
+        <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Node JS Form</title>
+    </head>
+    
+    <body>
+        <form action="/upload2" method="POST" enctype="multipart/form-data">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" />
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" />
+            </div>
+            <div>
+            <label for="avatar">Avatar</label>
+            <input type="file" name="avatar" id="avatar" />
+            </div>
+            <div>
+                <input type="submit" value="Register">
+            </div>
+        </form>
+    </body>
+    
+    </html>
+        `);
+    res.end();
+    //For simple form(and we need to change the html form action to "/" )
+  } else if (req.url === "/" && req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("close", () => {
+      const data = querystring.parse(body);
+      res.end();
+    });
+  } else if (req.url === "/upload" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("close", () => {
+      const dataBuffer = Buffer.concat(body);
+      const data = dataBuffer.toString("binary");
+      const boundary = req.headers["content-type"].split("boundary=").at(1);
+      const parts = data.split(`--${boundary}`);
+      const [meta, imageData] = parts[3].split(EOL + EOL);
+      const fileName = meta.match(/filename="(.+)"/)[1];
+      const savePath = path.join(__dirname, "uploads", fileName);
+      fs.writeFile(savePath, imageData, "binary", (err) => {
+        if (err) {
+          return res.end();
+        }
+        console.log("Image uploaded.");
+        res.writeHead(302, {
+          location: "/",
+        });
+        res.end();
+      });
+    });
+  } else if (req.url === "/upload2" && req.method === "POST") {
+     const form = new formidable.IncomingForm();
+     form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.end();
+        }
+         const savePath = path.join(__dirname, "uploads", files["avatar"].at(0).originalFilename)
+        fs.copyFile(files["avatar"].at(0).filepath, savePath, (err) => {
+           console.log("Image uploaded");
+           runInNewContext.writeHead(302, {
+            "location": "/"
+           })
+            res.end()
+        })
+       
+    })
+  }
+});
+
+server.listen(5000);
+console.log("Server is working on port 5000...");
